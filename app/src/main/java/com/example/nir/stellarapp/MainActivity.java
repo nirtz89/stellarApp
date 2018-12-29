f9d18ad42d278fcdfffc9a63ebd4d00226c2d5d0
Nir Tzezana <nirtz89@gmail.com>
2018-12-29 11:32:25 +0200

    Fragments!

--- app/src/main/java/com/example/nir/stellarapp/MainActivity.java
+++ app/src/main/java/com/example/nir/stellarapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nir.stellarapp;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.Window;
@@ -11,6 +12,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private StatePagerAdapter mStatePagerAdapter;
+    private ViewPager viewPager;
+
+
 //Roy Comment
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,13 +24,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    viewPager.setCurrentItem(0);
+//                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    viewPager.setCurrentItem(1);
+//                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    viewPager.setCurrentItem(2);
+//                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -37,9 +45,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        viewPager = findViewById(R.id.fragmentContainer);
+        setupViewPager(viewPager);
+//        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        StatePagerAdapter adapter = new StatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FragmentStories(), "Stories");
+//        adapter.addFragment(new FragmentStories(), "Stories");
+//        adapter.addFragment(new FragmentStories(), "Stories");
+        viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager(int fragmentNumber) {
+        viewPager.setCurrentItem(fragmentNumber);
     }
 
 }
